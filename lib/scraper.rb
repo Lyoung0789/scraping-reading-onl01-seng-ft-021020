496a8e518910ed9acc3e9e6e4ce69a5612c507b0
Lester Young <lyoung070789@gmail.com>
2020-02-26 18:31:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,5 +7,5 @@ doc = Nokogiri::HTML(html)
 courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
 courses.each do |course|
-  puts course.text.strip
+  puts course.text
 end 
